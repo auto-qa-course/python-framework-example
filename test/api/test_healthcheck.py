6a8a4547370723938e753d23fc1a5ef24a121fb0
Inna Ianko <inna.ianko@avid.com>
2018-02-08 16:14:39 +0200

    Renamed healthcheck test.

--- test/api/test_healthcheck.py
+++ test/api/test_healthcheck.py
@@ -4,7 +4,7 @@ from lib_custom.api.base_api import BaseApi
 
 class TestHealthcheck(TestBase):
 
-    def test_positive_contact_creation_check_response(self):
+    def test_service_healthcheck(self):
         base_api = BaseApi()
         url = '{}/healthcheck'.format(base_api.root_uri)
         response = base_api.get(url, headers={})
