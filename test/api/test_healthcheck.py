27e065b27203cb6badf58afa4995a606d16df4a8
Inna Ianko <inna.ianko@avid.com>
2018-02-08 16:13:15 +0200

    Minor fix in healthcheck test.

--- test/api/test_healthcheck.py
+++ test/api/test_healthcheck.py
@@ -6,7 +6,7 @@ class TestHealthcheck(TestBase):
 
     def test_positive_contact_creation_check_response(self):
         base_api = BaseApi()
-        url = '{}/healthchecks'.format(base_api.root_uri)
+        url = '{}/healthcheck'.format(base_api.root_uri)
         response = base_api.get(url, headers={})
         assert response.status_code == 200
         assert response._content == 'live'
